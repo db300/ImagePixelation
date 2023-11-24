c562bfebb49a9be1e4178d7e4ddab382a63ca102
DavidHawkes <db300@163.com>
2023-11-24 18:16:33 +0800

    创建项目

---
+++ ImageToPixel/MainForm.cs
@@ -0,0 +1,12 @@
+namespace ImageToPixel
+{
+    public partial class MainForm : Form
+    {
+        #region constructor
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        #endregion
+    }
+}
