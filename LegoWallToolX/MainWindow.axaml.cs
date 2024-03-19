8b61e9cdd7e7ddd846b5df6fbb169edcca678f94
DavidHawkes <db300@163.com>
2024-03-19 09:55:47 +0800

    测试nativemenu

--- LegoWallToolX/MainWindow.axaml.cs
+++ LegoWallToolX/MainWindow.axaml.cs
@@ -20,109 +20,104 @@ namespace LegoWallToolX
         }
         #endregion
 
+        #region menu
+        private async void NewFile()
+        {
+            var result = await new NewWindow().ShowDialog<FileItem>(this);
+            if (result is null) return;
+            for (var r = 0; r < result.RowCount; r++)
+            {
+                for (var c = 0; c < result.ColCount; c++)
+                {
+                    result.CanvasPixelColorItems.Add(new CanvasPixelColorItem { RowNum = r, ColNum = c, Color = result.BasePlateColor });
+                }
+            }
+            _mainCanvas.InitCanvas(result);
+        }
+
+        private async void OpenFile()
+        {
+            var topLevel = GetTopLevel(this);
+            if (topLevel is null) return;
+            var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+            {
+                FileTypeFilter = new List<FilePickerFileType>
+                {
+                    new("legox文件") { Patterns = new List<string> { "*.legox" } },
+                    new("所有文件") { Patterns = new List<string> { "*.*" } }
+                }
+            });
+            if (!(files?.Count > 0)) return;
+            var json = System.IO.File.ReadAllText(files[0].Path.LocalPath);
+            var fileItem = JsonConvert.DeserializeObject<FileItem>(json);
+            if (fileItem is null) return;
+            _mainCanvas.InitCanvas(fileItem);
+        }
+
+        private async void SaveFile()
+        {
+            var topLevel = GetTopLevel(this);
+            if (topLevel is null) return;
+            var file = await topLevel.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
+            {
+                DefaultExtension = "legox",
+                FileTypeChoices = new List<FilePickerFileType>
+                 {
+                     new("legox文件") { Patterns = new List<string> { "*.legox" } },
+                     new("所有文件") { Patterns = new List<string> { "*.*" } }
+                 }
+            });
+            if (file is null) return;
+            var fileItem = _mainCanvas.GetFileItem();
+            var json = JsonConvert.SerializeObject(fileItem);
+            System.IO.File.WriteAllText(file.Path.LocalPath, json);
+        }
+        #endregion
+
         #region event handler
-        private async void BtnNew_Click(object sender, RoutedEventArgs e)
+        private void BtnNew_Click(object sender, RoutedEventArgs e)
         {
-            var result = await new NewWindow().ShowDialog<FileItem>(this);
-            if (result is null) return;
-            for (var r = 0; r < result.RowCount; r++)
-            {
-                for (var c = 0; c < result.ColCount; c++)
-                {
-                    result.CanvasPixelColorItems.Add(new CanvasPixelColorItem { RowNum = r, ColNum = c, Color = result.BasePlateColor });
-                }
-            }
-            _mainCanvas.InitCanvas(result);
+            NewFile();
         }
 
-        private async void BtnOpen_Click(object sender, RoutedEventArgs e)
+        private void BtnOpen_Click(object sender, RoutedEventArgs e)
         {
-            var topLevel = GetTopLevel(this);
-            if (topLevel is null) return;
-            var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
-            {
-                FileTypeFilter = new List<FilePickerFileType>
-                {
-                    new("legox文件") { Patterns = new List<string> { "*.legox" } },
-                    new("所有文件") { Patterns = new List<string> { "*.*" } }
-                }
-            });
-            if (!(files?.Count > 0)) return;
-            var json = System.IO.File.ReadAllText(files[0].Path.LocalPath);
-            var fileItem = JsonConvert.DeserializeObject<FileItem>(json);
-            if (fileItem is null) return;
-            _mainCanvas.InitCanvas(fileItem);
+            OpenFile();
         }
 
-        private async void BtnSave_Click(object sender, RoutedEventArgs e)
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
         {
-            var topLevel = GetTopLevel(this);
-            if (topLevel is null) return;
-            var file = await topLevel.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
-            {
-                DefaultExtension = "legox",
-                FileTypeChoices = new List<FilePickerFileType>
-                 {
-                     new("legox文件") { Patterns = new List<string> { "*.legox" } },
-                     new("所有文件") { Patterns = new List<string> { "*.*" } }
-                 }
-            });
-            if (file is null) return;
-            var fileItem = _mainCanvas.GetFileItem();
-            var json = JsonConvert.SerializeObject(fileItem);
-            System.IO.File.WriteAllText(file.Path.LocalPath, json);
+            SaveFile();
         }
 
-        private async void MenuNew_Click(object? sender, RoutedEventArgs e)
+        private void MenuNew_Click(object? sender, RoutedEventArgs e)
         {
-            var result = await new NewWindow().ShowDialog<FileItem>(this);
-            if (result is null) return;
-            for (var r = 0; r < result.RowCount; r++)
-            {
-                for (var c = 0; c < result.ColCount; c++)
-                {
-                    result.CanvasPixelColorItems.Add(new CanvasPixelColorItem { RowNum = r, ColNum = c, Color = result.BasePlateColor });
-                }
-            }
-            _mainCanvas.InitCanvas(result);
+            NewFile();
         }
 
-        private async void MenuOpen_Click(object? sender, RoutedEventArgs e)
+        private void MenuOpen_Click(object? sender, RoutedEventArgs e)
         {
-            var topLevel = GetTopLevel(this);
-            if (topLevel is null) return;
-            var files = await topLevel.StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
-            {
-                FileTypeFilter = new List<FilePickerFileType>
-                {
-                    new("legox文件") { Patterns = new List<string> { "*.legox" } },
-                    new("所有文件") { Patterns = new List<string> { "*.*" } }
-                }
-            });
-            if (!(files?.Count > 0)) return;
-            var json = System.IO.File.ReadAllText(files[0].Path.LocalPath);
-            var fileItem = JsonConvert.DeserializeObject<FileItem>(json);
-            if (fileItem is null) return;
-            _mainCanvas.InitCanvas(fileItem);
+            OpenFile();
         }
 
-        private async void MenuSave_Click(object? sender, RoutedEventArgs e)
+        private void MenuSave_Click(object? sender, RoutedEventArgs e)
         {
-            var topLevel = GetTopLevel(this);
-            if (topLevel is null) return;
-            var file = await topLevel.StorageProvider.SaveFilePickerAsync(new FilePickerSaveOptions
-            {
-                DefaultExtension = "legox",
-                FileTypeChoices = new List<FilePickerFileType>
-                 {
-                     new("legox文件") { Patterns = new List<string> { "*.legox" } },
-                     new("所有文件") { Patterns = new List<string> { "*.*" } }
-                 }
-            });
-            if (file is null) return;
-            var fileItem = _mainCanvas.GetFileItem();
-            var json = JsonConvert.SerializeObject(fileItem);
-            System.IO.File.WriteAllText(file.Path.LocalPath, json);
+            SaveFile();
+        }
+
+        private void NativeMenuNew_Click(object? sender, System.EventArgs e)
+        {
+            NewFile();
+        }
+
+        private void NativeMenuOpen_Click(object? sender, System.EventArgs e)
+        {
+            OpenFile();
+        }
+
+        private void NativeMenuSave_Click(object? sender, System.EventArgs e)
+        {
+            SaveFile();
         }
         #endregion
 
@@ -160,25 +155,6 @@ namespace LegoWallToolX
 #if DEBUG
                 System.Diagnostics.Debug.WriteLine("当前操作系统是OSX");
 #endif
-                // 创建一个新的NativeMenu对象
-                var menu = new NativeMenu();
-
-                // 添加一个新的菜单项
-                var fileMenu = new NativeMenuItem("_File");
-                menu.Items.Add(fileMenu);
-
-                // 在_File菜单下添加一个子菜单项
-                var exitItem = new NativeMenuItem("E_xit");
-                fileMenu.Menu = new NativeMenu();
-                fileMenu.Menu.Items.Add(exitItem);
-
-                // 为退出菜单项添加点击事件
-                exitItem.Click += (s, e) =>
-                {
-                    // 在这里添加你的退出代码
-                };
-
-                // 将创建的菜单设置为应用程序的主菜单
             }
         }
         #endregion
